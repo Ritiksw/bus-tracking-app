35b7dda26a532843bc135e7045faf20038ad9bd1
Ritik Wandale <wandaleritik101@gmail.com>
2025-11-23 01:29:54 +0530

    fixing backend errors for date time

--- backend/src/main/java/com/bustracker/BusTrackingApiApplication.java
+++ backend/src/main/java/com/bustracker/BusTrackingApiApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableConfigurationProperties({JwtProperties.class, AdminBootstrapProperties.class})
 public class BusTrackingApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(BusTrackingApiApplication.class, args);
     }
 }
